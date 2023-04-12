55c865548818fe851cb2c03ceabfe03ffa2d63d0
Michael Warsitz <warsitzm@gmail.com>
2023-04-12 20:50:13 +0200

    filterExcelResults: Documentation added

--- ExcelReaderScript/evaluate_excel.py
+++ ExcelReaderScript/evaluate_excel.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 
-def readExcelFiles():
+def readExcelFiles() -> list:
     """This function returns a list of all excel files that are in the same
        folder.
 
@@ -18,9 +18,20 @@ def readExcelFiles():
     return excel_list
 
 
-def filterExcelResults(excel_file, filter):
+def filterExcelResults(excel_file, result_filter) -> object:
+    """This function reads the given excel file and filters the content
+       based on the given filter. The filtered data frame will be returned.
+
+    Args:
+        excel_file (str): Name of the excel file.
+        filter (dict): Filter as dictionary with header name as key and
+                       the value as value.
+
+    Returns:
+        object: Filtered data frame.
+    """
     df = pd.read_excel(excel_file, sheet_name='Tabelle1')
-    for filter_element in filter.items():
+    for filter_element in result_filter.items():
         header, value = filter_element
         if ' ' in header:
             header = '`' + header + '`'
@@ -28,11 +39,22 @@ def filterExcelResults(excel_file, filter):
     return df
 
 
+def extract_results_from_dataframe(df, variable_filter):
+    pass
+
+
+# Test programm
 result_filter = {'Testcase verdict': ['PASSED', 'FAILED'],
                  'Testcase name': ['Testcase_1', 'Testcase_2', 'Testcase_5'],
                  'R_Variable3': list(range(0, 3))
                  }
 
+variable_filter = {
+    'Testcase_1': ['R_Variable2', 'R_Variable3']
+}
+
+
 readExcelFiles()
 df_test = filterExcelResults('Testcase_1_date.xlsx', result_filter)
 print(df_test)
+extract_results_from_dataframe(df_test, variable_filter)
