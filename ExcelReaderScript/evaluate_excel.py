d398e7418abc71fec0a4d23f83fefaf722eaae3c
Michael Warsitz <warsitzm@gmail.com>
2023-04-17 01:00:37 +0200

    getAverageValue created

--- ExcelReaderScript/evaluate_excel.py
+++ ExcelReaderScript/evaluate_excel.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# import statistics
+from statistics import mean
 import os
 
 
@@ -18,7 +18,7 @@ def readExcelFiles() -> list:
     return excel_list
 
 
-def filterExcelResults(excel_file, result_filter) -> object:
+def filterExcelResults(df, result_filter) -> object:
     """This function reads the given excel file and filters the content
        based on the given filter. The filtered data frame will be returned.
 
@@ -30,13 +30,35 @@ def filterExcelResults(excel_file, result_filter) -> object:
     Returns:
         object: Filtered data frame.
     """
-    df = pd.read_excel(excel_file, sheet_name='Tabelle1')
     for filter_element in result_filter.items():
         header, value = filter_element
         if ' ' in header:
             header = '`' + header + '`'
         df.query('{0} in {1}'.format(header, value), inplace=True)
     return df
+
+
+def merge_exel_files_to_dataframe(df1, df2):
+    df_merged = pd.concat([df1, df2], ignore_index=True, sort=False)
+    return df_merged
+
+
+def getSuccessRatio(df, variable_filter):
+    pass
+
+
+def getAverageValue(df, testcase_filter, variable_filter):
+    df_filtered = filterExcelResults(df, testcase_filter)
+    average = list(df_filtered[variable_filter].mean())
+    return print(mean(average))
+
+
+def getMinimumValue(df, variable_filter):
+    pass
+
+
+def getMaximumValue(df, variable_filter):
+    pass
 
 
 def extract_results_from_dataframe(df, variable_filter):
@@ -46,15 +68,22 @@ def extract_results_from_dataframe(df, variable_filter):
 # Test programm
 result_filter = {'Testcase verdict': ['PASSED', 'FAILED'],
                  'Testcase name': ['Testcase_1', 'Testcase_2', 'Testcase_5'],
-                 'R_Variable3': list(range(0, 3))
+                 'R_Variable3': list(range(0, 20))
                  }
 
-variable_filter = {
-    'Testcase_1': ['R_Variable2', 'R_Variable3']
+testcase_filter = {
+    'Testcase name': ['Testcase_1', 'Testcase_5'],
 }
 
+variable_filter = ['R_Variable3', 'R_Variable4']
 
-readExcelFiles()
-df_test = filterExcelResults('Testcase_1_date.xlsx', result_filter)
-print(df_test)
-extract_results_from_dataframe(df_test, variable_filter)
+
+all_files = readExcelFiles()
+print(all_files)
+df_all_data = None
+for file in all_files:
+    df = pd.read_excel(file, sheet_name='Tabelle1')
+    df = filterExcelResults(df, result_filter)
+    df_all_data = merge_exel_files_to_dataframe(df_all_data, df)
+print(df_all_data)
+getAverageValue(df_all_data, testcase_filter, variable_filter)
