01ff8767272819e7491705aa6141bbe8a5c6f967
Michael Warsitz <warsitzm@gmail.com>
2023-05-22 22:39:35 +0200

    evaluate_excel: main programm finished ui: first version created

---
+++ ExcelReaderScript/ui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+# Create title of the window
+window.title('Test Result Calculator')
+# Set size of the window
+window.minsize(width=500, height=500)
+
+# Loop for waiting for interaction of the user
+window.mainloop()
