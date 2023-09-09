d27e974b326c80a005cfe9b75c98cd33f21939c6
Michael Warsitz <warsitzm@gmail.com>
2023-09-09 23:41:37 +0200

    Updates

--- ExcelReaderScript/Test.py
+++ ExcelReaderScript/Test.py
@@ -1,94 +1,30 @@
+import tkinter as tk
+from tkinter import ttk
 
-import pandas as pd
+# Create the main window
+root = tk.Tk()
+root.title("Fruit Selector")
 
+# Create a label
+label = tk.Label(root, text="Select a fruit:")
+label.pack(pady=10)
 
-# def write_dataframes_to_excel(df1, df2, file_name):
-#     # Erstelle einen Excel-Writer
-#     writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+# Create a StringVar to store the selected fruit
+selected_fruit = tk.StringVar()
 
-#     # Schreibe die DataFrames in separate Arbeitsblätter
-#     df1.to_excel(writer, sheet_name='DataFrame1', index=False)
-#     df2.to_excel(writer, sheet_name='DataFrame1', index=False, startrow=len(df1) + 2)  # Starte unterhalb von df1
+# Create a dropdown menu
+fruits = ["Apple", "Banana", "Orange"]
+fruit_dropdown = ttk.Combobox(root, textvariable=selected_fruit, values=fruits)
+fruit_dropdown.pack()
 
-#     # Erhalte das xlsxwriter-Objekt, um die Arbeitsblätter anzupassen
-#     # workbook = writer.book
-#     # worksheet1 = writer.sheets['DataFrame1']
-#     # worksheet2 = writer.sheets['DataFrame2']
+# Function to handle the selection
+def on_fruit_select(event):
+    selected = selected_fruit.get()
+    if selected:
+        label.config(text=f"You selected: {selected}")
 
-#     # Füge Formatierungen hinzu, um die Überschrift zu betonen
-#     # header_format = workbook.add_format({'bold': True, 'text_wrap': True, 'align': 'center', 'valign': 'vcenter'})
-#     # worksheet1.set_row(0, None, header_format)
-#     # worksheet2.set_row(0, None, header_format)
+# Bind the selection event to the function
+fruit_dropdown.bind("<<ComboboxSelected>>", on_fruit_select)
 
-#     # Schließe den Excel-Writer
-#     writer.save()
-
-# # Beispiel DataFrames
-# data1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-# data2 = {'X': [7, 8, 9], 'Y': [10, 11, 12]}
-# df1 = pd.DataFrame(data1)
-# df2 = pd.DataFrame(data2)
-
-# # Dateinamen
-# excel_file_name = 'dataframes_combined.xlsx'
-
-# # Rufe die Funktion auf, um die DataFrames in die Excel-Datei zu schreiben
-# write_dataframes_to_excel(df1, df2, excel_file_name)
-
-# def transform_strings_to_list(lst_string):
-#     value_list = []
-#     lst_string = lst_string[1:-1]
-#     start = 0
-#     end = 0
-#     while end != len(lst_string)-1:
-#         start = lst_string.find('[')
-#         end = lst_string.find(']')
-#         if end == -1:
-#             break
-#         string = lst_string[start+2:end].replace("'", "").split(',')
-#         value_list.append(string)
-#         lst_string = lst_string[end+1:]
-#     print(value_list)
-#     return value_list
-def extractValuesFromListOfStringLists(value_list, key, *args):
-    # Extract lists from the string lists and combine them to a single list
-    extracted_value_list = []
-    for string_value in value_list:
-        transformed_list = transformStringListsToList(string_value)
-        for list_value in transformed_list:
-            if list_value[0] == key:
-                extracted_value_list.append(float(list_value[1]))
-            else:
-                extracted_value_list.append(None)
-    return extracted_value_list
-
-
-def transformStringListsToList(string_list):
-    value_list = []
-    string_list = string_list[1:-1]
-    start = 0
-    end = 0
-    while end != len(string_list)-1:
-        start = string_list.find('[')
-        end = string_list.find(']')
-        if end == -1:
-            break
-        string = string_list[start+2:end].replace("'", "").split(',')
-        value_list.append(string)
-        string_list = string_list[end+1:]
-    return value_list
-
-
-lists = []
-key = 'Test'
-list_yo = ["[['Willkommen', 3.786], ['Test', 1.234]]", "[['Willkommen', 5.786]]", "[['Willkommen', 2.786]]"]
-print(isinstance(list_yo[0], str))
-print(list_yo[0][:2])
-val_list = extractValuesFromListOfStringLists(list_yo, key)
-print(val_list)
-
-import os
-
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
+# Start the Tkinter main loop
+root.mainloop()
--- ExcelReaderScript/UiConfigurateFilterAdd.py
+++ ExcelReaderScript/UiConfigurateFilterAdd.py
@@ -1,93 +1,13 @@
 import tkinter as tk
 import json
 import UiConfigurateFilter
+import UiElementCreator
 from evaluate_excel import read_filter_json
-
-
-class InputLineCreator:
-    instances_dict = {
-        'testcases': {
-            'col1': [],
-            'col2': [],
-            'row': 1
-        },
-        'calc_value': {
-            'col1': [],
-            'col2': [],
-            'row': 1
-        }
-    }
-
-    def __init__(self, frame, input_type) -> None:
-        self.text_var_col1 = tk.StringVar()
-        self.text_var_col2 = tk.StringVar()
-        self.input_line_col1 = tk.Entry(frame, textvariable=self.text_var_col1,
-                                        width=60)
-        self.input_line_col1.grid(column=0,
-                                  row=InputLineCreator.instances_dict[input_type]['row'],
-                                  sticky='we', padx=10, pady=(0, 5))
-        InputLineCreator.instances_dict[input_type]['col1'].append(self)
-        self.input_line_col2 = tk.Entry(frame, textvariable=self.text_var_col2,
-                                        width=60)
-        self.input_line_col2.grid(column=1,
-                                  row=InputLineCreator.instances_dict[input_type]['row'],
-                                  sticky='we', padx=10, pady=(0, 5))
-        InputLineCreator.instances_dict[input_type]['col2'].append(self)
-
-        InputLineCreator.instances_dict[input_type]['row'] += 1
-
-    def get_all_instances(input_type) -> list:
-        """This function returns a list that contains the entry line pairs of
-           given input type of the class as tupels.
-
-        Returns:
-            list: A list of all instances of the class that were instantiated.
-        """
-        col1 = InputLineCreator.instances_dict[input_type]['col1']
-        col2 = InputLineCreator.instances_dict[input_type]['col2']
-        return list(zip(col1, col2))
-
-    def delete_entry_line(self: object, col: str):
-        """This function deletes the given class object.
-
-        Args:
-            self (object): Instance of the class that will be deleted.
-        """
-        if col == 'col1':
-            self.input_line_col1.destroy()
-        elif col == 'col2':
-            self.input_line_col2.destroy()
-
-    def delete_obj_instances_dict(input_type, col, obj):
-        InputLineCreator.instances_dict[input_type][col].remove(obj)
-
-    def get_text(self, col):
-        if col == 'col1':
-            return self.text_var_col1.get()
-        elif col == 'col2':
-            return self.text_var_col2.get()
-
-    def insert_text_entry_obj(self: object, col: str, text: str):
-        if col == 'col1':
-            self.input_line_col1.insert(0, text)
-        if col == 'col2':
-            self.input_line_col2.insert(0, text)
-
-    def clean_up_objects():
-        """This function removes all objects from the instances dict and sets
-           the row number to 1.
-        """
-        InputLineCreator.instances_dict['testcases']['col1'] = []
-        InputLineCreator.instances_dict['testcases']['col2'] = []
-        InputLineCreator.instances_dict['testcases']['row'] = 1
-        InputLineCreator.instances_dict['calc_value']['col1'] = []
-        InputLineCreator.instances_dict['calc_value']['col2'] = []
-        InputLineCreator.instances_dict['calc_value']['row'] = 1
 
 
 def initialize_page_add(window, filter_file):
     # Clean up input line objects of the InputLineCreator class
-    InputLineCreator.clean_up_objects()
+    UiElementCreator.InputLineCreator.clean_up_objects()
     # Create a frame for the page
     page = tk.Frame(window)
     page.grid(row=0, column=0, sticky='nsew')
@@ -140,7 +60,7 @@ def initialize_page_add(window, filter_file):
                               font=('Arial', 8, 'bold'))
     variable_label.grid(row=0, column=1, sticky='w', padx=10, pady=5)
     # Input lines for testcase and variable
-    InputLineCreator(new_testcase_frame, 'testcases')
+    UiElementCreator.InputLineCreator(new_testcase_frame, 'testcases')
 
     # Frame for value to calculate entry and header in template entry
     new_calcvalue_frame = tk.Frame(new_entry_frame, relief='ridge')
@@ -155,7 +75,7 @@ def initialize_page_add(window, filter_file):
                             font=('Arial', 8, 'bold'))
     header_label.grid(row=0, column=1, sticky='w', padx=10, pady=5)
     # Input line for values to be calculated and header in template
-    InputLineCreator(new_calcvalue_frame, 'calc_value')
+    UiElementCreator.InputLineCreator(new_calcvalue_frame, 'calc_value')
 
     # Frame for options
     options_frame = tk.LabelFrame(page, text='Options', bd=2,
@@ -192,37 +112,37 @@ def initialize_page_add(window, filter_file):
 
 
 def add_entry_line(frame, input_type):
-    InputLineCreator(frame, input_type)
+    UiElementCreator.InputLineCreator(frame, input_type)
 
 
 def del_last_entry_line(input_type):
-    all_instances = InputLineCreator.get_all_instances(input_type)
+    all_instances = UiElementCreator.InputLineCreator.get_all_instances(input_type)
     if len(all_instances) < 2:
         return None
-    last_entry_line = InputLineCreator.get_all_instances(input_type)[-1]
-    InputLineCreator.delete_entry_line(last_entry_line[0], 'col1')
-    InputLineCreator.delete_obj_instances_dict(input_type, 'col1',
-                                               last_entry_line[0])
-    InputLineCreator.delete_entry_line(last_entry_line[1], 'col2')
-    InputLineCreator.delete_obj_instances_dict(input_type, 'col2',
-                                               last_entry_line[1])
-    InputLineCreator.instances_dict[input_type]['row'] -= 1
+    last_entry_line = UiElementCreator.InputLineCreator.get_all_instances(input_type)[-1]
+    UiElementCreator.InputLineCreator.delete_entry_line(last_entry_line[0], 'col1')
+    UiElementCreator.InputLineCreator.delete_obj_instances_dict(input_type, 'col1',
+                                                                last_entry_line[0])
+    UiElementCreator.InputLineCreator.delete_entry_line(last_entry_line[1], 'col2')
+    UiElementCreator.InputLineCreator.delete_obj_instances_dict(input_type, 'col2',
+                                                                last_entry_line[1])
+    UiElementCreator.InputLineCreator.instances_dict[input_type]['row'] -= 1
 
 
 def save_entry(filter_file, entry_name_input, window):
     # Get the texts of all testcase/variable inputs
     tc_var_dict = {}
-    tc_var = InputLineCreator.get_all_instances('testcases')
+    tc_var = UiElementCreator.InputLineCreator.get_all_instances('testcases')
     for instance in tc_var:
-        testcase = InputLineCreator.get_text(instance[0], 'col1')
-        variable = InputLineCreator.get_text(instance[1], 'col2')
+        testcase = UiElementCreator.InputLineCreator.get_text(instance[0], 'col1')
+        variable = UiElementCreator.InputLineCreator.get_text(instance[1], 'col2')
         tc_var_dict[testcase] = variable
     # Get the texts of all value to calculate/template header inputs
     valcalc_tempheader_dict = {}
-    valcalc_tempheader = InputLineCreator.get_all_instances('calc_value')
+    valcalc_tempheader = UiElementCreator.InputLineCreator.get_all_instances('calc_value')
     for instance in valcalc_tempheader:
-        valcalc = InputLineCreator.get_text(instance[0], 'col1')
-        tempheader = InputLineCreator.get_text(instance[1], 'col2')
+        valcalc = UiElementCreator.InputLineCreator.get_text(instance[0], 'col1')
+        tempheader = UiElementCreator.InputLineCreator.get_text(instance[1], 'col2')
         valcalc_tempheader_dict[valcalc] = tempheader
     # Load the filter data from json file
     filter_data = read_filter_json(filter_file)
